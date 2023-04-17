5932d0a8e7e633ab7ec407930957e19902497fc6
Satish Yadav <satish.yadav@within-inc.co.in>
2023-04-17 13:02:50 +0530

    code setuo

--- src/modules/Counter/Counter.js
+++ src/modules/Counter/Counter.js
@@ -1,24 +1,41 @@
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-  decrementCount,
-  incrementCount,
-  resetCount,
+    decrementCount,
+    incrementCount,
+    resetCount
 } from 'shared/redux/commonActions/commonActions';
 
 function Counter() {
-  const { counter } = useSelector((state) => state?.sharedInfo);
-  const dispatch = useDispatch();
+    const { counter } = useSelector((state) => state?.sharedInfo);
+    const dispatch = useDispatch();
 
-  return (
-    <div>
-      <h1>
-        Count: <span>{counter}</span>
-      </h1>
-      <button onClick={() => dispatch(incrementCount())}>+1</button>
-      <button onClick={() => dispatch(decrementCount())}>-1</button>
-      <button onClick={() => dispatch(resetCount())}>Reset</button>
-    </div>
-  );
+    useEffect(() => {
+        if (counter === 0) {
+            dispatch(incrementCount());
+        }
+    });
+
+    const [abc, setAbc] = useState('');
+
+    useEffect(() => {
+        if (abc === 'abc') {
+            setAbc('ddd');
+        }
+    });
+
+    var a = 'abc';
+
+    return (
+        <div>
+            <h1>
+                Count: <span>{counter}</span>
+            </h1>
+            <button onClick={() => dispatch(incrementCount())}>+1</button>
+            <button onClick={() => dispatch(decrementCount())}>-1</button>
+            <button onClick={() => dispatch(resetCount())}>Reset</button>
+        </div>
+    );
 }
 
 export default Counter;
